68562e41dafbd01b4d5c91dc6152fd7ca7216b1b
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-04 22:57:43 +0530

    GT(feat) : stylings in user admin page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,35 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { DeleteModelComponent } from '../delete-model/delete-model.component';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { DeleteModelComponent } from "../delete-model/delete-model.component";
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.scss"],
 })
 export class UsersComponent implements OnInit {
+  userData = [
+    {
+      empid: 1,
+      lastName: "Nallamothu",
+      firstName: "Sudharshan",
+      role: "IT",
+      status: "ACTIVE",
+      email: "sudharshan.nallamothu@oracle.com",
+    },
+    {
+      empid: 1,
+      lastName: "Nallamothu",
+      firstName: "Sudharshan",
+      role: "IT",
+      status: "ACTIVE",
+      email: "sudharshan.nallamothu@oracle.com",
+    },
+    {
+      empid: 1,
+      lastName: "Nallamothu",
+      firstName: "Sudharshan",
+      role: "IT",
+      status: "ACTIVE",
+      email: "sudharshan.nallamothu@oracle.com",
+    },
+    {
+      empid: 1,
+      lastName: "Nallamothu",
+      firstName: "Sudharshan",
+      role: "IT",
+      status: "IN-ACTIVE",
+      email: "sudharshan.nallamothu@oracle.com",
+    },
+  ];
+  constructor(private router: Router, public dialog: MatDialog) {}
 
-  constructor(private router:Router,public dialog: MatDialog) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addUser() {
+    this.router.navigate(["add-user"]);
   }
-
-  addUser(){
-    this.router.navigate(['add-user']);   
+  download(type) {
+    console.log(type);
   }
+  deleteModel() {
+    const dialogRef = this.dialog.open(DeleteModelComponent, {
+      width: "25%",
+      data: "user",
+    });
 
-
-    
-deleteModel(){
-  const dialogRef = this.dialog.open(DeleteModelComponent, {
-    width: '25%',
-    data:"user"
-  });
-
-  dialogRef.afterClosed().subscribe(result => {
-    console.log(`Dialog result: ${result}`);
-  });
-}
-
-
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
