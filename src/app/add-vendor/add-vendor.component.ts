cd8f8582ef26d4e6526d440907c0552c28f22c42
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-06 01:04:32 +0530

    GT(Feat) : add vendor form

--- src/app/add-vendor/add-vendor.component.ts
+++ src/app/add-vendor/add-vendor.component.ts
@@ -1,51 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { SuccessfulModelComponent } from '../successful-model/successful-model.component';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { SuccessfulModelComponent } from "../successful-model/successful-model.component";
 @Component({
-  selector: 'app-add-vendor',
-  templateUrl: './add-vendor.component.html',
-  styleUrls: ['./add-vendor.component.scss']
+  selector: "app-add-vendor",
+  templateUrl: "./add-vendor.component.html",
+  styleUrls: ["./add-vendor.component.scss"],
 })
 export class AddVendorComponent implements OnInit {
- addVendorGroup:FormGroup
-  constructor(private fb:FormBuilder, public dialog: MatDialog,private router:Router) { 
+  addVendorGroup: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    public dialog: MatDialog,
+    private router: Router
+  ) {
     this.addVendorGroup = this.fb.group({
-      "storename": ['Enter Full Store Name',Validators.required],
-      "category": ['',Validators.required],
-      "phone":['Enter Phone Number',Validators.required],
-      "email":['Enter Email Id',Validators.required],
-      "gst":['Enter GST Number',Validators.required],
-      "pan":['Enter Pan Number',Validators.required],
-      "country":['',Validators.required],
-      "state":['',Validators.required],
-      "city":['',Validators.required],
-      "locality":['',Validators.required],
-      "address":['Enter Address',Validators.required],
-      "managername" :['Enter Manager Name',Validators.required],
-      "manageremail" :['Enter Manager Email Id',Validators.required],
-      "managerPhone" :['Enter Manager Phone',Validators.required],
-      "aboutstore":['',Validators.required],
-     
-    })
+      storename: ["", Validators.required],
+      category: ["", Validators.required],
+      phone: ["", Validators.required],
+      email: ["", Validators.required],
+      gst: ["", Validators.required],
+      pan: ["", Validators.required],
+      country: ["", Validators.required],
+      state: ["", Validators.required],
+      city: ["", Validators.required],
+      locality: ["", Validators.required],
+      address: ["", Validators.required],
+      managername: ["", Validators.required],
+      manageremail: ["", Validators.required],
+      managerPhone: ["", Validators.required],
+      storeId: ["", Validators.required],
+      aboutstore: ["", Validators.required],
+    });
   }
 
-  ngOnInit(): void {
-  }
-  addVendorSubmit(){
-    const dialogRef = this.dialog.open(SuccessfulModelComponent ,{
-      data:"vendor"
+  ngOnInit(): void {}
+  addVendorSubmit() {
+    const dialogRef = this.dialog.open(SuccessfulModelComponent, {
+      data: "vendor",
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
-  
-    console.log('this.addVendorGroup.value',this.addVendorGroup.value)
-  }
-  onBoarding(){
-    this.router.navigate(['vendor-policies-view']);   
-  }
 
- 
+    console.log("this.addVendorGroup.value", this.addVendorGroup.value);
+  }
+  onBoarding() {
+    this.router.navigate(["vendor-policies-view"]);
+  }
 }
