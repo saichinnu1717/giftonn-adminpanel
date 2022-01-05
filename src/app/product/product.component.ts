41fd66b69111fc40e0f23c221ac69543325ffafb
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-06 04:27:33 +0530

    GT(feat) : product page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,43 +1,136 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { ApprovedModelComponent } from '../approved-model/approved-model.component';
-import { DenyProductComponent } from '../deny-product/deny-product.component';
-import { FreezedModelComponent } from '../freezed-model/freezed-model.component';
-import { SuccessfulModelComponent } from '../successful-model/successful-model.component';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { ApprovedModelComponent } from "../approved-model/approved-model.component";
+import { DenyProductComponent } from "../deny-product/deny-product.component";
+import { FreezedModelComponent } from "../freezed-model/freezed-model.component";
+import { SuccessfulModelComponent } from "../successful-model/successful-model.component";
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.scss"],
 })
 export class ProductComponent implements OnInit {
+  products = [
+    {
+      name: "Lorme",
+      storeName: "Joyalukkas",
+      productId: "JOY1234",
+      category: "Jewellery",
+      subCategory: "Ring",
+      gender: "Female",
+      description: "Description",
+      price: "1000",
+      taxes: "100",
+      totalPrice: "1100",
+      noOfUnits: "30",
+      status: "Pending",
+      image: "../../../../assets/gift.jpeg",
+      video: "https://www.youtube.com/embed/n2Tl_eECPIM",
+    },
+    {
+      name: "Lorme",
+      storeName: "Joyalukkas",
+      productId: "JOY1234",
+      category: "Jewellery",
+      subCategory: "Ring",
+      gender: "Female",
+      description: "Description",
+      price: "1000",
+      taxes: "100",
+      totalPrice: "1100",
+      noOfUnits: "30",
+      status: "Pending",
+      image: "../../../../assets/gift.jpeg",
+      video: "https://www.youtube.com/embed/n2Tl_eECPIM",
+    },
+    {
+      name: "Lorme",
+      storeName: "Joyalukkas",
+      productId: "JOY1234",
+      category: "Jewellery",
+      subCategory: "Ring",
+      gender: "Female",
+      description: "Description",
+      price: "1000",
+      taxes: "100",
+      totalPrice: "1100",
+      noOfUnits: "30",
+      status: "Pending",
+      image: "../../../../assets/gift.jpeg",
+      video: "https://www.youtube.com/embed/n2Tl_eECPIM",
+    },
+    {
+      name: "Lorme",
+      storeName: "Joyalukkas",
+      productId: "JOY1234",
+      category: "Jewellery",
+      subCategory: "Ring",
+      gender: "Female",
+      description: "Description",
+      price: "1000",
+      taxes: "100",
+      totalPrice: "1100",
+      noOfUnits: "30",
+      status: "Pending",
+      image: "../../../../assets/gift.jpeg",
+      video: "https://www.youtube.com/embed/n2Tl_eECPIM",
+    },
+    {
+      name: "Lorme",
+      storeName: "Joyalukkas",
+      productId: "JOY1234",
+      category: "Jewellery",
+      subCategory: "Ring",
+      gender: "Female",
+      description: "Description",
+      price: "1000",
+      taxes: "100",
+      totalPrice: "1100",
+      noOfUnits: "30",
+      status: "Pending",
+      image: "../../../../assets/gift.jpeg",
+      video: "https://www.youtube.com/embed/n2Tl_eECPIM",
+    },
+    {
+      name: "Lorme",
+      storeName: "Joyalukkas",
+      productId: "JOY1234",
+      category: "Jewellery",
+      subCategory: "Ring",
+      gender: "Female",
+      description: "Description",
+      price: "1000",
+      taxes: "100",
+      totalPrice: "1100",
+      noOfUnits: "30",
+      status: "Approve",
+      image: "../../../../assets/gift.jpeg",
+      video: "https://www.youtube.com/embed/n2Tl_eECPIM",
+    },
+  ];
+  constructor(private router: Router, public dialog: MatDialog) {}
 
-  constructor(private router:Router,public dialog: MatDialog) { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  addProduct() {
+    this.router.navigate(["add-product"]);
   }
-addProduct(){
-    this.router.navigate(['add-product']);   
+  approveProduct(product) {
+    const dialogRef = this.dialog.open(ApprovedModelComponent, {
+      width: "30%",
+      data: "product",
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
-  action(value){
-    console.log(value)
-    if(value=='deny'){
-      const dialogRef = this.dialog.open(DenyProductComponent,{
-        width:"50%",
-        data:"product"
-       });
-       dialogRef.afterClosed().subscribe(result => {
-         console.log(`Dialog result: ${result}`);
-       });
-     }
-     if(value=='approve'){
-      const dialogRef = this.dialog.open(ApprovedModelComponent,{
-        width:"30%",
-        data:"product"
-       });
-       dialogRef.afterClosed().subscribe(result => {
-         console.log(`Dialog result: ${result}`);
-       });
-     }
-    }
+  denyProduct(product) {
+    const dialogRef = this.dialog.open(DenyProductComponent, {
+      width: "30%",
+      data: "product",
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
