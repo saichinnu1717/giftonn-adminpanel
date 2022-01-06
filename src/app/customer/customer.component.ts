ffae7b9e65f49541cc25c877796976151ed2600b
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-06 15:51:23 +0530

    GT(fix) : table responsive

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,40 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { FreezeRoleComponent } from '../freeze-role/freeze-role.component';
-import { RatingForCustomerComponent } from '../rating-for-customer/rating-for-customer.component';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { FreezeRoleComponent } from "../freeze-role/freeze-role.component";
+import { RatingForCustomerComponent } from "../rating-for-customer/rating-for-customer.component";
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  selector: "app-customer",
+  templateUrl: "./customer.component.html",
+  styleUrls: ["./customer.component.scss"],
 })
 export class CustomerComponent implements OnInit {
-  mymodel
-  constructor(public dialog: MatDialog) { }
+  customers = [
+    {
+      name: "Rajesh Sharma",
+      customerId: "12345",
+      emailId: "test@gmail.com",
+      mobileNumber: "1234567890",
+      country: "India",
+      location: "Hyderabad",
+      dateTime: "10-12-2021",
+      rating: "4.5",
+      status: "pending",
+    },
+    {
+      name: "Rajesh Sharma",
+      customerId: "12345",
+      emailId: "test@gmail.com",
+      mobileNumber: "1234567890",
+      country: "India",
+      location: "Hyderabad",
+      dateTime: "10-12-2021",
+      rating: "4.5",
+      status: "Approved",
+    },
+  ];
+  constructor(public dialog: MatDialog) {}
 
-  ngOnInit(): void {
-  }
-  action(value){
-    console.log(value)
-    if(value=='rating'){
-      const dialogRef = this.dialog.open(RatingForCustomerComponent,{
-        width:"50%",
-        data:"customer"
-       });
-       dialogRef.afterClosed().subscribe(result => {
-         console.log(`Dialog result: ${result}`);
-       });
-     }
-     if(value=='freeze'){
-      const dialogRef = this.dialog.open(FreezeRoleComponent,{
-        width:"50%",
-        data:"customer"
-       });
-       dialogRef.afterClosed().subscribe(result => {
-         console.log(`Dialog result: ${result}`);
-       });
-     }
+  ngOnInit(): void {}
+  action(value) {
+    console.log(value);
+    if (value == "rating") {
+      const dialogRef = this.dialog.open(RatingForCustomerComponent, {
+        width: "50%",
+        data: "customer",
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log(`Dialog result: ${result}`);
+      });
+    }
+    if (value == "freeze") {
+      const dialogRef = this.dialog.open(FreezeRoleComponent, {
+        width: "50%",
+        data: "customer",
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log(`Dialog result: ${result}`);
+      });
     }
   }
+}
 
 //FreezeRoleComponent
