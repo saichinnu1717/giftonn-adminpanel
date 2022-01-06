87fb6c7dfab54163001c6ed47165ebb0b969a754
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-06 14:06:58 +0530

    GT(feat) : product add form

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,52 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { SuccessfulModelComponent } from '../successful-model/successful-model.component';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { SuccessfulModelComponent } from "../successful-model/successful-model.component";
 @Component({
-  selector: 'app-add-product',
-  templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  selector: "app-add-product",
+  templateUrl: "./add-product.component.html",
+  styleUrls: ["./add-product.component.scss"],
 })
 export class AddProductComponent implements OnInit {
-  addProductGroup:FormGroup
+  addProductGroup: FormGroup;
   submitted = false;
-  constructor(private fb:FormBuilder, public dialog: MatDialog) {
-    this.addProductGroup =  this.fb.group({
-      "productId": ['',Validators.required],
-      "productName": ['',Validators.required],
-      "category": ['',Validators.required],
-      "subcategory":['',Validators.required],
-      "price": ['',Validators.required],
-      "clarity": ['',Validators.required],
-      "type": ['',Validators.required],
-      "goldpurity": ['',Validators.required],
-      "size": ['',Validators.required],
-      "taxes": ['',Validators.required],
-      "unit": ['',Validators.required],
-      "gender": ['',Validators.required],
-      "totalprice": ['',Validators.required],
-      "productdescription":['',Validators.required],
-      "productimage":['',Validators.required],
-      "productvedio":['',],
-    })
-   }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+    this.addProductGroup = this.fb.group({
+      productId: ["", Validators.required],
+      storeName: ["", Validators.required],
+      productName: ["", Validators.required],
+      category: ["", Validators.required],
+      subcategory: ["", Validators.required],
+      price: ["", Validators.required],
+      clarity: ["", Validators.required],
+      type: ["", Validators.required],
+      goldpurity: ["", Validators.required],
+      size: ["", Validators.required],
+      taxes: ["", Validators.required],
+      unit: ["", Validators.required],
+      gender: ["", Validators.required],
+      totalprice: ["", Validators.required],
+      productdescription: ["", Validators.required],
+      productimage: ["", Validators.required],
+      productvedio: [""],
+    });
   }
+
+  ngOnInit(): void {}
   get f() {
     return this.addProductGroup.controls;
   }
- addProductSubmit(){
+  addProductSubmit() {
     this.submitted = true;
     if (this.addProductGroup.invalid) {
       return;
     }
-    const dialogRef = this.dialog.open(SuccessfulModelComponent ,{
-      data:"product"
+    const dialogRef = this.dialog.open(SuccessfulModelComponent, {
+      data: "product",
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
-    console.log('this.addProductGroup.value',this.addProductGroup.value)
+    console.log("this.addProductGroup.value", this.addProductGroup.value);
   }
 }
