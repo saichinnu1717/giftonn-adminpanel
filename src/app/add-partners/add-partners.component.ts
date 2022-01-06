09fe8b7eaec060f0bd16b7886c45d7ca6215ede2
Sudharshan Nallamothu <sudharshan.nallamotthu@oracle.com>
2022-01-06 18:54:49 +0530

    GT(feat) : add partner

--- src/app/add-partners/add-partners.component.ts
+++ src/app/add-partners/add-partners.component.ts
@@ -1,30 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-add-partners',
-  templateUrl: './add-partners.component.html',
-  styleUrls: ['./add-partners.component.scss']
+  selector: "app-add-partners",
+  templateUrl: "./add-partners.component.html",
+  styleUrls: ["./add-partners.component.scss"],
 })
 export class AddPartnersComponent implements OnInit {
-  addPartnersGroup:FormGroup
-  constructor(private fb:FormBuilder) { 
+  addPartnersGroup: FormGroup;
+  submitted = false;
+  constructor(private fb: FormBuilder) {
     this.addPartnersGroup = this.fb.group({
-      "partnersName":['',Validators.required],
-      "partnersEmail":['',Validators.required],
-      "partnersCategory":['',Validators.required],
-      "partnersSubCategory":['',Validators.required], 
-      "contactNumber":['',Validators.required], 
-      "country":['',Validators.required],
-      "state":['',Validators.required],  
-      "city":['',Validators.required], 
-    })
+      partnersName: ["", Validators.required],
+      partnersEmail: ["", Validators.required],
+      partnersCategory: ["", Validators.required],
+      partnersSubCategory: ["", Validators.required],
+      contactNumber: ["", Validators.required],
+      country: ["", Validators.required],
+      state: ["", Validators.required],
+      city: ["", Validators.required],
+      productimage: ["", Validators.required],
+    });
   }
 
-  ngOnInit(): void {
-  }
-  addPartnersSubmit(){
-    console.log(this.addPartnersGroup.value)
+  ngOnInit(): void {}
+  addPartnersSubmit() {
+    this.submitted = true;
+    console.log(this.addPartnersGroup.value);
   }
   handleChange(files: FileList) {}
 }
