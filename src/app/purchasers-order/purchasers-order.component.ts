c8188f4deb41dca4671c540f3e345336675edd8a
Sai Krishna <saichinnu1717@gmail.com>
2022-01-10 16:58:11 +0530

    purchasers chnage

--- src/app/purchasers-order/purchasers-order.component.ts
+++ src/app/purchasers-order/purchasers-order.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { MatDialog } from "@angular/material/dialog";
 import { ApprovedModelComponent } from "../approved-model/approved-model.component";
 import { DenyProductComponent } from "../deny-product/deny-product.component";
 import { FreezeRoleComponent } from "../freeze-role/freeze-role.component";
+import { SuccessfulModelComponent } from "../successful-model/successful-model.component";
 
 @Component({
   selector: "app-purchasers-order",
@@ -10,64 +12,97 @@ import { FreezeRoleComponent } from "../freeze-role/freeze-role.component";
   styleUrls: ["./purchasers-order.component.scss"],
 })
 export class PurchasersOrderComponent implements OnInit {
+  generateOpt: FormGroup;
+  generateQr: FormGroup;
   Orders = [
     {
-      orderId: "test",
-      customerId: "test",
-      storeId: "test",
-      purchasername: "test",
-      category: "test",
-      subCategory: "test",
-      productId: "test",
-      productName: "test",
-      netPrice: "test",
-      totalPrice: "test",
+      orderId: "258664",
+      customerId: "258586",
+      storeId: "225466",
+      purchasername: "Sai Krishna",
+      category: "Jewellery",
+      subCategory: "Bracelet",
+      productId: "JOY56882",
+      productName: "Bracelet",
+      netPrice: "15000",
+      totalPrice: "16000",
       status: "Pending",
     },
     {
-      orderId: "test",
-      customerId: "test",
-      storeId: "test",
-      purchasername: "test",
-      category: "test",
-      subCategory: "test",
-      productId: "test",
-      productName: "test",
-      netPrice: "test",
-      totalPrice: "test",
+      orderId: "258664",
+      customerId: "258586",
+      storeId: "225466",
+      purchasername: "Sai Krishna",
+      category: "Jewellery",
+      subCategory: "Bracelet",
+      productId: "JOY56882",
+      productName: "Bracelet",
+      netPrice: "15000",
+      totalPrice: "16000",
       status: "Refunded",
     },
     {
-      orderId: "test",
-      customerId: "test",
-      storeId: "test",
-      purchasername: "test",
-      category: "test",
-      subCategory: "test",
-      productId: "test",
-      productName: "test",
-      netPrice: "test",
-      totalPrice: "test",
+      orderId: "258664",
+      customerId: "258586",
+      storeId: "225466",
+      purchasername: "Sai Krishna",
+      category: "Jewellery",
+      subCategory: "Bracelet",
+      productId: "JOY56882",
+      productName: "Bracelet",
+      netPrice: "15000",
+      totalPrice: "16000",
       status: "Confirmed",
     },
     {
-      orderId: "test",
-      customerId: "test",
-      storeId: "test",
-      purchasername: "test",
-      category: "test",
-      subCategory: "test",
-      productId: "test",
-      productName: "test",
-      netPrice: "test",
-      totalPrice: "test",
+      orderId: "258664",
+      customerId: "258586",
+      storeId: "225466",
+      purchasername: "Sai Krishna",
+      category: "Jewellery",
+      subCategory: "Bracelet",
+      productId: "JOY56882",
+      productName: "Bracelet",
+      netPrice: "15000",
+      totalPrice: "16000",
       status: "Cancelled",
     },
+
   ];
-  constructor(public dialog: MatDialog) {}
+  coupons = [{
+    code: "ASDVM56",
+    storeid: "JOY2255",
+    storeName: "Joyalukkas",
+    couponType: "flat",
+    amount: "500",
+    category: "jewellary",
+    subCategort: "Bracelet",
 
-  ngOnInit(): void {}
+  }]
+  constructor(public dialog: MatDialog, private fb: FormBuilder,) {
+    this.generateOpt = this.fb.group({
+      orderid: ["", Validators.required],
+      confirmedOtp: ["", Validators.required]
+    });
+    this.generateQr = this.fb.group({
+      qrNumber: ["", Validators.required],
+      purchasername: ["", Validators.required],
+      productName: ["", Validators.required],
+      productId: ["", Validators.required],
+      SubCategory: ["", Validators.required],
+      category: ["", Validators.required],
+      storeid: ["", Validators.required],
+      mobileNumber: ["", Validators.required],
+    });
+  }
 
+  ngOnInit(): void { }
+  QrSubmit() {
+    console.log("this.generateQr.value", this.generateQr.value);
+  }
+  OptSubmit() {
+    console.log("this.generateOpt.value", this.generateOpt.value);
+  }
   action(value) {
     console.log(value);
     if (value == "freeze") {
