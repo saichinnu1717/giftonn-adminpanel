21dfa3681a985a8ccbe4921469c2080f0e89500b
Sai Krishna <saichinnu1717@gmail.com>
2022-01-11 14:58:36 +0530

    vender order

--- src/app/vender-order/vender-order.component.ts
+++ src/app/vender-order/vender-order.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { ApprovedModelComponent } from "../approved-model/approved-model.component";
+import { DenyProductComponent } from "../deny-product/deny-product.component";
+import { FilterComponent } from "../filter/filter.component";
+import { FreezedModelComponent } from "../freezed-model/freezed-model.component";
+import { SuccessfulModelComponent } from "../successful-model/successful-model.component";
 
 @Component({
   selector: 'app-vender-order',
@@ -6,10 +13,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vender-order.component.scss']
 })
 export class VenderOrderComponent implements OnInit {
+  vendorList = [
+    {
+      qrnumber: "21558",
+      orderId: "225222",
+      storeId: "55665",
+      CustomerId: "85545",
+      purchaserName: "Sai Krishna",
+      dateTime: "12/01/2022 || 1:30",
+      productId: "JOY1244",
+      productName: "Bracelet",
+      totalPrice: "15000",
+      discount: "25%",
+      netPrice: "14500",
 
-  constructor() { }
+    },
+    {
+      qrnumber: "21558",
+      orderId: "225222",
+      storeId: "55665",
+      CustomerId: "85545",
+      purchaserName: "Sai Krishna",
+      dateTime: "12/01/2022 || 1:30",
+      productId: "JOY1244",
+      productName: "Bracelet",
+      totalPrice: "15000",
+      discount: "25%",
+      netPrice: "14500",
+
+    },
+    {
+      qrnumber: "21558",
+      orderId: "225222",
+      storeId: "55665",
+      CustomerId: "85545",
+      purchaserName: "Sai Krishna",
+      dateTime: "12/01/2022 || 1:30",
+      productId: "JOY1244",
+      productName: "Bracelet",
+      totalPrice: "15000",
+      discount: "25%",
+      netPrice: "14500",
+
+    },]
+
+  constructor(private router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  openFilter() {
+    const dialogRef = this.dialog.open(FilterComponent, {
+      width: "50%",
+      data: { filterType: "vendorordeForm" },
+    });
+  }
+  approveProduct(vendorordeFormr) {
+    const dialogRef = this.dialog.open(ApprovedModelComponent, {
+      width: "30%",
+      data: "vendorordeForm",
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+  denyProduct(vendorordeForm) {
+    const dialogRef = this.dialog.open(DenyProductComponent, {
+      width: "50%",
+      data: "vendorordeForm",
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
